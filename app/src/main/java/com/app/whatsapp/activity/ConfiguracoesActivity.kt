fadb56edf37bfdb817611ece289f40b987a00ee2
andre <andreluccag@gmail.com>
2023-02-02 20:49:52 -0300

    add permissões e validando

--- app/src/main/java/com/app/whatsapp/activity/ConfiguracoesActivity.kt
+++ app/src/main/java/com/app/whatsapp/activity/ConfiguracoesActivity.kt
@@ -1,8 +1,11 @@
 package com.app.whatsapp.activity
 
 import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.app.whatsapp.databinding.ActivityConfiguracoesBinding
 import com.app.whatsapp.helper.Permissao
 
@@ -22,5 +25,29 @@ class ConfiguracoesActivity : AppCompatActivity() {
         binding.toolbar.toolbarPrincipal.title = "Configurações"
         setSupportActionBar(binding.toolbar.toolbarPrincipal)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        for (permissaoResultado : Int in grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_GRANTED)
+                alertaValidacaoPermissao()
+        }
+    }
+
+    private fun alertaValidacaoPermissao() {
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Permissões negadas")
+        builder.setMessage("Para utilizar o app é necessário caeitar as permissões!")
+        builder.setCancelable(false)
+        builder.setPositiveButton("Confirmar", DialogInterface.OnClickListener { dialog, which ->
+            finish()
+        })
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
