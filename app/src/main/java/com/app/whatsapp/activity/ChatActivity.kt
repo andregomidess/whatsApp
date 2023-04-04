31d47b437457cbbe9c816779edcfb2124b3093b6
andregomidess <andreluccag@gmail.com>
2023-04-04 18:47:42 -0300

    criando tela de chat

---
+++ app/src/main/java/com/app/whatsapp/activity/ChatActivity.kt
@@ -0,0 +1,42 @@
+package com.app.whatsapp.activity
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.app.whatsapp.R
+//import com.app.whatsapp.activity.databinding.ActivityChatBinding
+import com.app.whatsapp.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityChatBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_chat)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_chat)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
--- app/src/main/java/com/app/whatsapp/fragments/ContatosFragment.kt
+++ app/src/main/java/com/app/whatsapp/fragments/ContatosFragment.kt
@@ -1,21 +1,29 @@
 package com.app.whatsapp.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.app.whatsapp.R
+import com.app.whatsapp.activity.ChatActivity
 import com.app.whatsapp.adapter.ContatosAdapter
 import com.app.whatsapp.config.ConfiguracaoFirebase
+import com.app.whatsapp.helper.RecyclerItemClickListener
+import com.app.whatsapp.helper.RecyclerItemClickListener.*
+import com.app.whatsapp.helper.UsuarioFirebase
 import com.app.whatsapp.model.Usuario
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
+
 
 class ContatosFragment : Fragment() {
 
@@ -24,6 +32,7 @@ class ContatosFragment : Fragment() {
     val listaContatos = ArrayList<Usuario>()
     private lateinit var usuarioRef: DatabaseReference
     private lateinit var valueEventListenerContatos: ValueEventListener
+    private lateinit var usuarioAtual: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +49,7 @@ class ContatosFragment : Fragment() {
         recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos)
 
         usuarioRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuario")
+        usuarioAtual = UsuarioFirebase.getUsuarioFirebase()
 
 
         //config adapter
@@ -50,6 +60,25 @@ class ContatosFragment : Fragment() {
         recyclerViewListaContatos.layoutManager = layoutManager
         recyclerViewListaContatos.setHasFixedSize(true)
         recyclerViewListaContatos.adapter = adapter
+
+        //config evento de clique no recyclerview
+        recyclerViewListaContatos.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                activity,
+                recyclerViewListaContatos,
+                object : OnItemClickListener {
+                    override fun onItemClick(view: View?, position: Int) {
+                        val i: Intent = Intent(activity, ChatActivity::class.java)
+                        startActivity(i)
+                    }
+                    override fun onLongItemClick(view: View?, position: Int) {}
+                    override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+
+                    }
+                }
+            )
+        )
+
 
 
 
@@ -72,9 +101,11 @@ class ContatosFragment : Fragment() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 listaContatos.clear()
                 for (dados: DataSnapshot in snapshot.children){
-                    val usuario = dados.getValue(Usuario::class.java)
+
+                    val usuario: Usuario? = dados.getValue(Usuario::class.java)
                     if (usuario != null) {
-                        listaContatos.add(usuario)
+                        if (!usuarioAtual.email.equals(usuario.email))
+                            listaContatos.add(usuario)
                     }
                 }
                 adapter.notifyDataSetChanged()
